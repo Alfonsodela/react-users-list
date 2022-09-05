ebfee3ac0ca529008c528eac0484c915974f8087
Alfonsodela <89443967+Alfonsodela@users.noreply.github.com>
2022-09-05 14:07:20 +0200

    spread the props and keys

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,22 @@
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const app = <img src='/logo.svg' />;
+const getData = (name, rol) => (
+	<div>
+		<span>{name}</span>
+		<span>{rol}</span>
+	</div>
+)
+	
+
+
+const app = (
+	<div>
+		{getData('Alfonso', 'estudiante')}
+	</div>
+	
+)
+
 const container = document.getElementById('root');
 
 ReactDOM.render(app, container);
