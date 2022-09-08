5d16e4a1de5db6452b62209bea1bb81c9fb38bf9
Alfonsodela <89443967+Alfonsodela@users.noreply.github.com>
2022-09-08 09:28:12 +0200

    destructuring

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,8 @@
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const getData = (name, rol) => (
-	<div>
-		<span>{name}</span>
-		<span>{rol}</span>
-	</div>
-)
-	
 
-
-const app = (
-	<div>
-		{getData('Alfonso', 'estudiante')}
-	</div>
-	
-)
+const app = 
 
 const container = document.getElementById('root');
 
