14a93f34ad11c3872f386ccb81a69cbf45290f0a
Alfonsodela <89443967+Alfonsodela@users.noreply.github.com>
2022-09-12 20:48:52 +0200

    controlled form

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,17 +1,28 @@
 import UserRow from './UserRow';
 import style from './UsersList.module.css';
+import { useState } from 'react';
 
 const UsersList = ({ users, children }) => {
+	const [search, setSearch] = useState('');
+
+	const usersFiltered = search
+		? users.filter(user => user.name.toLowerCase().startsWith(search.toLowerCase()))
+		: users;
+
 	const usersRender =
-		users.length > 0 ? (
-			users.map(item => <UserRow key={UserRow.name} {...item} />)
+		usersFiltered.length > 0 ? (
+			usersFiltered.map(item => <UserRow key={UserRow.name} {...item} />)
 		) : (
 			<p>No hay usuarios</p>
 		);
 
 	return (
 		<div className={style.wrapper}>
-			{children} 
+			{children}
+			<input type='text' name='search' value={search} onChange={(ev) => setSearch(ev.target.value)}
+			>
+
+			</input>
 			{usersRender}
 		</div>
 	);
