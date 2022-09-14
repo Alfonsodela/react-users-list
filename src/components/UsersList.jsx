7a9c64dee5378d216a16563e80ca0bb630e1b639
Alfonsodela <89443967+Alfonsodela@users.noreply.github.com>
2022-09-14 22:34:25 +0200

    add new filters: filterActiveUsers, filterUsersByName, sortUsers

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -4,8 +4,12 @@ import { useState } from 'react';
 
 const UsersList = ({ users }) => {
 	const [search, setSearch] = useState('');
+	const [onlyActive, setOnlyActive] = useState(false);
+	const [sortBy, setSortBy] = useState(0);
 
-	const usersFiltered = filterUsersByName(users, search);
+	let usersFiltered = filterActiveUsers(users, onlyActive);
+	usersFiltered = filterUsersByName(usersFiltered, search);
+	usersFiltered = sortUsers(usersFiltered, sortBy);
 	const usersRender = renderUsers(usersFiltered);
 
 	return (
@@ -18,10 +22,14 @@ const UsersList = ({ users }) => {
 					onChange={ev => setSearch(ev.target.value)}
 				></input>
 				<div className={style.active}>
-					<input type='checkbox'></input>
+					<input
+						type='checkbox'
+						checked={onlyActive}
+						onChange={ev => setOnlyActive(ev.target.checked)}
+					></input>
 					<span>Sólo activos</span>
 				</div>
-				<select>
+				<select value={sortBy} onChange={ev => setSortBy(Number(ev.target.value))}>
 					<option value={0}>Por defecto</option>
 					<option value={1}>Por nombre</option>
 				</select>
@@ -41,10 +49,29 @@ const filterUsersByName = (users, search) => {
 	);
 };
 
+const filterActiveUsers = (users, active) => {
+	if (!active) return users;
+
+	return users.filter(user => user.active);
+};
+
 const renderUsers = users => {
 	if (users.length <= 0) return <p>No hay usuarios</p>;
 
 	return users.map(item => <UserRow key={item.name} {...item} />);
+};
+
+const sortUsers = (users, sortBy) => {
+	switch (sortBy) {
+		case 1:
+			return users.sort((a, b) => {
+				if (a.name > b.name) return 1;
+				if (a.name < b.name) return -1;
+			 return 0;
+			});
+		default:
+			return users;
+	}
 };
 
 export default UsersList;
