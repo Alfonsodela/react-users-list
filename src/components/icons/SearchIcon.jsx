4107a304ec68771cf8b74decc3a3cdf5d7fda6aa
Alfonsodela <89443967+Alfonsodela@users.noreply.github.com>
2022-09-19 11:16:49 +0200

    added logic and style of input search

--- src/components/icons/SearchIcon.jsx
+++ src/components/icons/SearchIcon.jsx
@@ -1,4 +1,4 @@
-const CheckCircleIcon = props => (
+const SearchIcon = props => (
 	<svg
 		{...props}
 		fill='none'
@@ -10,9 +10,9 @@ const CheckCircleIcon = props => (
 			strokeLinecap='round'
 			strokeLinejoin='round'
 			strokeWidth='2'
-			d='M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z'
+			d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
 		></path>
 	</svg>
 );
 
-export default CheckCircleIcon;
+export default SearchIcon;
