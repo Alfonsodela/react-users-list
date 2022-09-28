98bfee59fc5e0ee4fb00fe6397ac04e202408b04
Alfonsodela <89443967+Alfonsodela@users.noreply.github.com>
2022-09-28 19:58:28 +0200

    add onSubmit in UserCreateForm

--- src/components/user-forms/UserCreateForm.jsx
+++ src/components/user-forms/UserCreateForm.jsx
@@ -1,12 +1,5 @@
 import { USER_ROLE } from '../../constants/userRoles';
-// import {
-// 	nameChanged,
-// 	usernameChanged
-// } from '../../lib/actions/createFormActions';
-
-// import { UserFormsContext } from '../../lib/contexts/UserFormsContext';
-
-// import { useCreateForm } from '../../lib/hooks/useCreateForm';
+import { useCreateForm } from '../../lib/hooks/useCreateForm';
 import Button from '../buttons/Button';
 import IconButton from '../buttons/IconButton';
 import InputCheckbox from '../forms/InputCheckbox';
@@ -15,62 +8,93 @@ import InputTextAsync from '../forms/InputTextAsync';
 import Select from '../forms/Select';
 import style from './UserCreateForm.module.css';
 import CrossIcon from '../icons/CrossIcon';
-import { useCreateForm } from '../../lib/hooks/useCreateForm';
+import { useState } from 'react';
 
 const UserCreateForm = ({ onClose }) => {
+	const [isSubmitting, setIsSubmitting] = useState(false);
 	const { username, name, setUserName, setName } = useCreateForm();
-	// const { onSuccess } = useContext(UserFormsContext);
 
-	// const [isSubmitting, setIsSubmitting] = useState(false);
-	// const { username, name, dispatchFormValues, isFormInvalid } = useCreateForm();
+	const isDisabled =
+		!name.value ||
+		name.error ||
+		!username.value ||
+		username.error ||
+		username.loading ||
+		isSubmitting;
 
 	return (
-		<form
-			className={style.form}
-			// onSubmit={ev =>
-			// 	handleSubmit(ev, name, username, setIsSubmitting, onSuccess, closeModal)
-			// }
-		>
+		<div className={style.wrapper}>
 			<IconButton
 				className={style.close}
 				icon={CrossIcon}
 				filled
 				onClick={onClose}
 			/>
-			<div className={style.row}>
-				<InputText
-					className={style.input}
-					label='Nombre'
-					placeholder='John Doe'
-					error={name.error}
-					value={name.value}
-					onChange={ev => setName(ev.target.value)}
-				></InputText>
-				<InputTextAsync
-					className={style.input}
-					label='Username'
-					placeholder='johndoe'
-					success={username.value && !username.loading && !username.error}
-					loading={username.loading}
-					error={username.error}
-					value={username.value}
-					onChange={ev => setUserName(ev.target.value)}
-				></InputTextAsync>
-			</div>
-			<div className={style.row}>
-				<Select name='role'>
-					<option value={USER_ROLE.TEACHER}>Profesor</option>
-					<option value={USER_ROLE.STUDENT}>Alumno</option>
-					<option value={USER_ROLE.OTHER}>Otro</option>
-				</Select>
-				<div className={style.active}>
-					<InputCheckbox name='active' />
-					<span>¿Activo?</span>
+			<form onSubmit={ev => handleSubmit(ev, name, username, setIsSubmitting, onClose)}>
+				<div className={style.row}>
+					<InputText
+						className={style.input}
+						label='Nombre'
+						placeholder='John Doe'
+						error={name.error}
+						value={name.value}
+						onChange={ev => setName(ev.target.value)}
+					></InputText>
+					<InputTextAsync
+						className={style.input}
+						label='Username'
+						placeholder='johndoe'
+						success={username.value && !username.loading && !username.error}
+						loading={username.loading}
+						error={username.error}
+						value={username.value}
+						onChange={ev => setUserName(ev.target.value)}
+					></InputTextAsync>
 				</div>
-				<Button type='submit'>Crear usuario</Button>
-			</div>
-		</form>
+				<div className={style.row}>
+					<Select name='role'>
+						<option value={USER_ROLE.TEACHER}>Profesor</option>
+						<option value={USER_ROLE.STUDENT}>Alumno</option>
+						<option value={USER_ROLE.OTHER}>Otro</option>
+					</Select>
+					<div className={style.active}>
+						<InputCheckbox name='active' />
+						<span>¿Activo?</span>
+					</div>
+					<Button type='submit' disabled={isDisabled}>
+						{isSubmitting ? 'Cargando...' : 'Crear usuario'}
+					</Button>
+				</div>
+			</form>
+		</div>
 	);
+};
+
+const handleSubmit = async (ev, name, username, setIsSubmitting, onClose) => {
+	ev.preventDefault();
+
+	setIsSubmitting(true);
+
+	const user = {
+		id: crypto.randomUUID,
+		name: name.value,
+		username: username.value,
+		role: ev.target.role.value,
+		active: ev.target.active.checked
+	};
+
+	const res = await fetch('http://localhost:4000/users', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(user)
+	});
+
+	if (res.ok) {
+		onClose()
+	}
+	else { setIsSubmitting(false) }
 };
 
 export default UserCreateForm;
